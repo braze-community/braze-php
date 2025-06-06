d50cc7f4b5a13abfec8bdc7d2a35ad6747ae0ca1
Mark <mark@remarkablemark.org>
2025-06-05 22:44:35 -0400

    test: add SubscriptionUserStatusTest

---
+++ tests/SubscriptionUserStatusTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Braze\Braze;
+
+it('sends a GET to "/subscription/user/status"', function (): void {
+    $braze = new Braze('https://rest.iad-02.braze.com', 'API_KEY');
+
+    // https://www.braze.com/docs/api/endpoints/subscription_groups/get_list_user_subscription_groups/#request-parameters
+    $params = [
+        'external_id' => 'id1',
+        'email' => 'john@example.com',
+        'limit' => 100,
+        'offset' => 0,
+        'phone' => '+1234567890',
+    ];
+
+    try {
+        $braze->client->getSubscriptionUserStatus($params);
+    } catch (Throwable $throwable) {
+        expect($throwable->getMessage())->toBe('401 Unauthorized');
+    }
+});
